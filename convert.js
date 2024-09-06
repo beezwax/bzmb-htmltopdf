38f3804f6d90130791c50e652dc2424e89aafaa9
Alec Gregory <alec_g@beezwax.net>
2024-09-06 10:47:56 +0100

    Adjust

--- convert.js
+++ convert.js
@@ -44,7 +44,7 @@ module.exports = new Promise(function (resolve, reject) {
     })
     .catch((error) => {
       console.log(`error type: ${typeof error}`);
-      console.log(`error code: ${error.code}`);
+      console.log(`error keys: ${Object.keys(error)}`);
     })
 })
 
