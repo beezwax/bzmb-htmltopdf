404a76f600ca4dcd9b2a0669e9a3f0e59be4333a
Alec Gregory <alec_g@beezwax.net>
2024-09-10 22:21:49 +0100

    Fix running as root

--- convert.js
+++ convert.js
@@ -1,7 +1,7 @@
 const puppeteer = require("puppeteer");
 
 async function convert (html, options) {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({args: ["--no-sandbox"]});
   const page = await browser.newPage();
   await page.setContent(html);
   const pdf = await page.pdf(options);
